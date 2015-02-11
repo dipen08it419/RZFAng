b84e69b9204b04233285de3e9104bcef19228cd2
Dipen Shah <dipen08it419@gmail.com>
2015-02-11 04:17:01 -0800

    Moved content from regular site to angular application (excluding contact page).

---
+++ RoZeeFinancialAng/scripts/app/controllers.js
@@ -0,0 +1,9 @@
+﻿angular.module('RZFAng')
+.controller('NavigationController', ['$location', '$scope', function ($location, $scope) {
+
+    $scope.isActive = function (currentPath) {
+        if ($location.path().indexOf(currentPath)!=-1)
+            return true;
+        return false;
+    }
+}]);
